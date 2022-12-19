73fe98df3e61ca8b1809dfa5d1ecd0f96c084f40
Nikolina Novakovic <109579817+Ninolina88@users.noreply.github.com>
2022-12-19 09:12:19 +0100

    894/Nikolina/CRUD and postman collection completed (#2)

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,53 @@
 const router = require("express").Router();
 const prisma = require("../prisma");
 
-const todo = prisma.todo;
+const license = prisma.license;
+const domain = prisma.domain;
+const token = prisma.token;
+const app = prisma.app;
 
 router.use(
-  "/todo",
-  require("../rdms/crud")(todo, {
+  "/license",
+  require("../rdbms/crud")(license, {
+    middleware: {
+      create: [],
+      readMany: [],
+      readOne: [],
+      update: [],
+      remove: [],
+    },
+  })
+);
+
+router.use(
+  "/domain",
+  require("../rdbms/crud")(domain, {
+    middleware: {
+      create: [],
+      readMany: [],
+      readOne: [],
+      update: [],
+      remove: [],
+    },
+  })
+);
+
+router.use(
+  "/token",
+  require("../rdbms/crud")(token, {
+    middleware: {
+      create: [],
+      readMany: [],
+      readOne: [],
+      update: [],
+      remove: [],
+    },
+  })
+);
+
+router.use(
+  "/app",
+  require("../rdbms/crud")(app, {
     middleware: {
       create: [],
       readMany: [],
