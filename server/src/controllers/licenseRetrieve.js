514139f3adb2f2f0d1f2d2d1620f2ebad4d36fd0
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-12-26 09:47:32 +0100

    898/899/900 license retrieve using token or domain

---
+++ server/src/controllers/licenseRetrieve.js
@@ -0,0 +1,32 @@
+const prisma = require("../prisma");
+const license = prisma.license;
+
+const licenseRetrieve = () => async (req, res, next) => {
+  try {
+    const reqToken = req.token
+    const pickUpLicensesToken = await license.findUnique({
+      where: {
+        tokens: { token: reqToken },
+      },
+    });
+    if (!pickUpLicensesToken) {
+      const reqDomain = req.domain
+      const pickUpLicensesDomain = await license.findUnique({
+        where: {
+          domain: { name: reqDomain },
+        },
+      });
+      if (!pickUpLicensesDomain) {
+        throw new Error(
+          `no licenses were found for ${reqToken} token or ${reqDomain} domain`
+        );
+      }
+      res.status(200).json(pickUpLicensesDomain);
+    }
+    res.status(200).json(pickUpLicensesToken);
+  } catch {
+    res.status(403);
+  }
+};
+
+module.exports = licenseRetrieve;
--- server/src/middleware/clientParser.js
+++ server/src/middleware/clientParser.js
@@ -1,22 +1,38 @@
 const prisma = require("../prisma");
 const domain = prisma.domain;
+const token = prisma.token;
 
-const clientParser = async (req, res, next) => {
-  try {
-    const origin = req.headers.origin;
-
-    const clientDomain = await domain.findUnique({
-      where: { name: origin },
-    });
-    if (!clientDomain) {
-      throw Error("domain not found");
+  const clientParser = async (req, res, next) => {
+    try {
+      const sanitizeToken = req.headers.authorization;
+      if (!sanitizeToken) {
+        const origin = req.headers.origin;
+        if (!origin) {
+          throw Error("invalid request");
+        }
+        const clientDomain = await domain.findUnique({
+          where: { name: origin },
+        });
+        if (!clientDomain) {
+          throw Error("domain not found");
+        }
+        req.domain = clientDomain;
+        next();
+      }
+      const tokenStart = 7;
+      const sanitizedToken = sanitizeToken.substring(tokenStart, sanitizeToken.length);
+      const clientToken = await token.findUnique({
+        where: { token: sanitizedToken },
+      });
+      if (!clientToken) {
+        throw Error("token not found");
+      }
+      req.token = clientToken;
+      next();
+    } catch (error) {
+      res.status(403);
+      next(error);
     }
-    req.domain = clientDomain;
-    next();
-  } catch (error) {
-    res.status(400);
-    next(error);
-  }
-};
+  };
 
-module.exports = clientParser;
+  module.exports = clientParser;
