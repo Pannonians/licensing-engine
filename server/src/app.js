135ee872151841ebccec9871e852c3842ca67561
VPetar <freeze.988@gmail.com>
2023-04-12 10:15:11 +0200

    disable cors package entirely

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const helmet = require("helmet");
 var morgan = require("morgan");
 const errorMiddleware = require("./middleware/error.middleware");
@@ -18,7 +18,7 @@ var accessLogStream = rfs.createStream("access.log", {
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json({limit: '15mb'}));
 app.use(express.urlencoded({limit: '15mb'}));
 
